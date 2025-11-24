e8955bc3d0f586af3a94d3ab6fad897536f0bb85
enesyukselx <enesyuksel9903@gmail.com>
2025-11-24 14:37:02 +0300

    refactor(core): adjust coordinate transformation in screenToWorld and worldToScreen functions

--- packages/core/src/utils/screenToWorld.ts
+++ packages/core/src/utils/screenToWorld.ts
@@ -2,7 +2,7 @@ import { Coords } from "../types";
 
 export function screenToWorld(x: number, y: number, origin: Coords, scale: number): { x: number; y: number } {
     return {
-        x: x / scale + origin.x,
-        y: y / scale + origin.y,
+        x: x / scale + origin.x - 0.5,
+        y: y / scale + origin.y - 0.5,
     };
 }
--- packages/core/src/utils/worldToScreen.ts
+++ packages/core/src/utils/worldToScreen.ts
@@ -2,7 +2,7 @@ import { Coords } from "../types";
 
 export function worldToScreen(x: number, y: number, origin: Coords, scale: number): { x: number; y: number } {
     return {
-        x: (x - origin.x) * scale,
-        y: (y - origin.y) * scale,
+        x: (x + 0.5 - origin.x) * scale,
+        y: (y + 0.5 - origin.y) * scale,
     };
 }
