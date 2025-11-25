c0fc94d00ecc5eb9768b9cc5e7020c6676596eed
enesyukselx <enesyuksel9903@gmail.com>
2025-11-25 14:53:38 +0300

    chore(core): remove unused files

--- packages/core/src/utils/topLeftToCenter.ts
+++
@@ -1,7 +0,0 @@
-export function topLeftToCenterX(canvasWidth: number, scale: number, xCoord: number = 0): number {
-    return xCoord + canvasWidth / (2 * scale) - 0.5;
-}
-
-export function topLeftToCenterY(canvasHeight: number, scale: number, yCoord: number = 0): number {
-    return yCoord + canvasHeight / (2 * scale) - 0.5;
-}
--- packages/core/src/utils/worldToScreen.ts
+++
@@ -1,8 +0,0 @@
-import { Coords } from "../types";
-
-export function worldToScreen(x: number, y: number, origin: Coords, scale: number): { x: number; y: number } {
-    return {
-        x: (x + 0.5 - origin.x) * scale,
-        y: (y + 0.5 - origin.y) * scale,
-    };
-}
