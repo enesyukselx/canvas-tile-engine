3d73a1039b0c0213e3979eafd1e2eeb8ec950791
enesyukselx <enesyuksel9903@gmail.com>
2026-01-02 14:39:50 +0300

    done

---
+++ examples/react/responsive-game-map/src/constants.ts
@@ -0,0 +1,76 @@
+import Village1Webp from "/villages/village1.webp";
+import Village2Webp from "/villages/village2.webp";
+import Village3Webp from "/villages/village3.webp";
+import Village4Webp from "/villages/village4.webp";
+import Village5Webp from "/villages/village5.webp";
+import Village6Webp from "/villages/village6.webp";
+import Village1BarbarWebp from "/villages/village1_barbar.webp";
+import Village2BarbarWebp from "/villages/village2_barbar.webp";
+import Village3BarbarWebp from "/villages/village3_barbar.webp";
+import Village4BarbarWebp from "/villages/village4_barbar.webp";
+import Village5BarbarWebp from "/villages/village5_barbar.webp";
+import Village6BarbarWebp from "/villages/village6_barbar.webp";
+import ForestTerrainWebp from "/terrains/forest.webp";
+import LakeTerrainWebp from "/terrains/lake.webp";
+import MineTerrainWebp from "/terrains/mine.webp";
+
+export const COLORS = ["red", "blue", "purple"] as const;
+export type Color = (typeof COLORS)[number];
+
+export const VILLAGE_TYPES = ["normal", "barbar"] as const;
+export type VillageType = (typeof VILLAGE_TYPES)[number];
+
+export const VILLAGE_SIZES = [1, 2, 3, 4, 5, 6] as const;
+export type VillageSize = (typeof VILLAGE_SIZES)[number];
+
+export const TERRAINS = ["forest", "lake", "mine"] as const;
+export type Terrain = (typeof TERRAINS)[number];
+
+export const NORMAL_VILLAGE_IMAGES: Record<VillageSize, string> = {
+    1: Village1Webp,
+    2: Village2Webp,
+    3: Village3Webp,
+    4: Village4Webp,
+    5: Village5Webp,
+    6: Village6Webp,
+};
+
+export const BARBAR_VILLAGE_IMAGES: Record<VillageSize, string> = {
+    1: Village1BarbarWebp,
+    2: Village2BarbarWebp,
+    3: Village3BarbarWebp,
+    4: Village4BarbarWebp,
+    5: Village5BarbarWebp,
+    6: Village6BarbarWebp,
+};
+
+export const TERRAIN_IMAGES: Record<Terrain, string> = {
+    forest: ForestTerrainWebp,
+    lake: LakeTerrainWebp,
+    mine: MineTerrainWebp,
+};
+
+export const INITIAL_COORDS = { x: 200, y: 200 };
+export const MAP_BACKGROUND_COLOR = "#337426ff";
+
+export interface SizeOption {
+    value: string;
+    label: string;
+}
+
+export const MINI_MAP_SIZE_OPTIONS: SizeOption[] = [
+    { value: "100", label: "100x100" },
+    { value: "200", label: "200x200" },
+    { value: "300", label: "300x300" },
+    { value: "400", label: "400x400" },
+    { value: "500", label: "500x500" },
+];
+
+export const MAIN_MAP_SIZE_OPTIONS: SizeOption[] = [
+    { value: "300", label: "300x300" },
+    { value: "400", label: "400x400" },
+    { value: "500", label: "500x500" },
+    { value: "600", label: "600x600" },
+    { value: "700", label: "700x700" },
+    { value: "800", label: "800x800" },
+];
